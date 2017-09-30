3154129c13f4eb5103b8fbdfcc894dba3e680c14
Harry <harry@h313.info>
2017-09-30 17:18:47 -0400

    javadoc people
    don't be lazy

--- Node.java
+++ Node.java
@@ -1,21 +1,50 @@
+/**
+ * Generic node class
+ * 
+ * @author  Somebody idk
+ * @version 0.0.0
+ */
 public class Node <T>
 {
     T value;
     Node next;
     
+    /**
+     * A default node constructor
+     * @param val a value to be stored in the node
+     */
     public Node(T val) {
         value = val;
     }
     
+    /**
+     * A node getter
+     * @return the value stored by the node
+     */
     public T get() {
         return value;
     }
+    
+    /**
+     * A setter for the node's value
+     * @param v a value for the node to hold
+     */
     public void set(T v) {
         value = v;
     }
+    
+    /**
+     * Getter for the next node
+     * @return the next node
+     */
     public Node getNext() {
         return next;
     }
+    
+    /**
+     * Setter for the next node
+     * @param n the next node to be set
+     */
     public void setNext(Node n) {
         next = n;
     }
