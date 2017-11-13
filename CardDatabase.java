b9fd16058ba068282f84c652bedd9f856632be0c
Christina Kim <kim.christina@charterschool.org>
2017-11-13 08:08:31 -0500

    More of Matt's Stuff

--- CardDatabase.java
+++ CardDatabase.java
@@ -7,9 +7,12 @@
  */
 public class CardDatabase
 {
-    public Card[] database = new Card[6];
-
-    public CardDatabase() {
-        database[0] = new Card("slime", 0, 0, 2, 1, "slime.png");
+ 
+    public static Card CardDatabase(int n) {
+        Card[] database = new Card[3];
+        database[0] = new Card("Slime", 0, 0, 2, 1, "slime.png");
+        database[1] = new Card("Default Hero", 1, 2, 1, 1,"defaulthero.png");
+        database[2] = new Card("Grand Slampire", 2, 13, 9, 5, "grandslampire.png");
+        return database[n];
     }
 }
