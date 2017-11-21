465e59498ac89b523062e6c301d053cdd9a2c92a
Josh <fickes.joshua@charterschool.org>
2017-11-21 08:40:46 -0500

    Combat Works!
    There is now an additional column on the right which contains (from top to bottom):
    Player 1 HP, A useless button where we can display "Player 1/2 Wins!" at the end of the game, An End Turn Button (not yet functional), and Player 2 HP
    
    If a minion attacks an empty space, it will instead attack your opponent directly!!  So combat works!
    
    What we need now is a turn system (just make a turn variable, and in that bottom most function, so add a condition so top rows only trigger when turn is 1, and bottom rows only trigger when turn is 2.  Make the end turn button switch turn and draw a card.  We also need to find the leftmost open spot in a row so we can add cards to the hand and play minions in the leftmost open spot, instead of directly ahead.  Make sure that when checking for leftmost open spot, you ignore the rightmost column.  We also need mana, and a way to make cards only be abgle to attack once per turn (probably just add a boolean variable to the card object that gets refereshed by the end turn button, as well as a couple of conditions in combat).

--- buttonGame.java
+++ buttonGame.java
@@ -9,10 +9,12 @@ import javax.imageio.ImageIO;
  */
 public class buttonGame extends JFrame {
     static int rows = 4;
-    static int columns = 5;
-    static Card[][] card = new Card[4][5];
-    static JButton[][] buttons = new JButton[4][5];
+    static int columns = 6; //Rightmost column is end turn button and player health displays.
+    static Card[][] card = new Card[4][6];
+    static JButton[][] buttons = new JButton[4][6];
     static JFrame p = new JFrame();
+    static int p1HP = 50;
+    static int p2HP = 50;
 
     public static void main(String[] args) {
         drawBoard();
@@ -27,23 +29,30 @@ public class buttonGame extends JFrame {
                 for (int j = 0; j < columns; j++) {
                     int rand = (int) (Math.random() * 3);
                     card[i][j] = CardDatabase.CardDatabase(rand);
+                    
                 }
             }
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < columns; j++) {                    
                     ImageIcon img = new ImageIcon(card[i][j].getCardImage());
+                    
                     JButton b = new JButton("Button" + i + "," + j);
                     b.addActionListener(new ButtonListener());
-                    if (i == 3 || i == 0) {
+                    if (j != 5) {
                         b.setIcon(img);
                     }
-                    else if (i == 1 && j == 1) {
+                    
+                    /*else if (i == 1 && j == 1) {
                         b.setIcon(img);
-                    }
+                    }*/
                     p.add(b);
                     buttons[i][j] = b;
                 }
             }
+            buttons[2][5].setText("End Turn");
+            buttons[0][5].setText(p1HP + "");
+            buttons[3][5].setText(p2HP + "");
+            buttons[1][5].setText("Game Name Here");
         }
 
         catch (Exception e) {
@@ -70,60 +79,79 @@ public class buttonGame extends JFrame {
 
     public static void combat(int a, int c) {
         if (a == 1) {
-            String card1 = card[a][c].toString();
-            String card2 = card[a + 1][c].toString();
-            System.out.println(card1 + " has attack/defense " + card[a][c].getAttack() + "/" + card[a][c].getDefense());
-            System.out.println(card2 + " has attack/defense " + card[a + 1][c].getAttack() + "/" + card[a + 1][c].getDefense());
-            System.out.println(card1 + " takes " + card[a+1][c].getAttack() + " damage");
-            System.out.println(card2 + " takes " + card[a][c].getAttack() + " damage");
-            
-            boolean card1dead = false;
-            boolean card2dead = false;
-            if (card[a][c].getAttack() >= card[a + 1][c].getDefense()) {
-                System.out.println(card2 + " dies");
-                card2dead = true;
-            }
-            if (card[a][c].getDefense() <= card[a + 1][c].getAttack()) {
-                System.out.println(card1 + " dies");
-                card1dead = true;
-            }
-            
-            //This should make combat matter.
-            if (card2dead) {
-                buttons[a+1][c].setIcon(new ImageIcon("NoCard.png"));
-                card[a+1][c] = null;
-            }
-            if (card1dead) {
-                buttons[a][c].setIcon(new ImageIcon("NoCard.png"));
-                card[a][c] = null;
+            if (card[a + 1][c] == null) {
+                System.out.println("nulled");
+                String card1 = card[a][c].toString();
+                System.out.println(card1 + " has attack/defense " + card[a][c].getAttack() + "/" + card[a][c].getDefense());
+                System.out.println(card1 + " deals " + card[a][c].getAttack() + " damage player 2!");
+                p2HP = p2HP - card[a][c].getAttack();
+                buttons[3][5].setText(p2HP+"");
+            } else {
+                System.out.println("not nulled");
+                String card1 = card[a][c].toString();
+                String card2 = card[a + 1][c].toString();
+                System.out.println(card1 + " has attack/defense " + card[a][c].getAttack() + "/" + card[a][c].getDefense());
+                System.out.println(card2 + " has attack/defense " + card[a + 1][c].getAttack() + "/" + card[a + 1][c].getDefense());
+                System.out.println(card1 + " takes " + card[a+1][c].getAttack() + " damage");
+                System.out.println(card2 + " takes " + card[a][c].getAttack() + " damage");
+                
+                boolean card1dead = false;
+                boolean card2dead = false;
+                if (card[a][c].getAttack() >= card[a + 1][c].getDefense()) {
+                    System.out.println(card2 + " dies");
+                    card2dead = true;
+                }
+                if (card[a][c].getDefense() <= card[a + 1][c].getAttack()) {
+                    System.out.println(card1 + " dies");
+                    card1dead = true;
+                }
+                
+                //This should make combat matter.
+                if (card2dead) {
+                    buttons[a+1][c].setIcon(new ImageIcon("NoCard.png"));
+                    card[a+1][c] = null;
+                }
+                if (card1dead) {
+                    buttons[a][c].setIcon(new ImageIcon("NoCard.png"));
+                    card[a][c] = null;
+                }
             }
         } else if (a == 2) {
-            String card1 = card[a][c].toString();
-            String card2 = card[a - 1][c].toString();
-            System.out.println(card1 + " has attack/defense " + card[a][c].getAttack() + "/" + card[a][c].getDefense());
-            System.out.println(card2 + " has attack/defense " + card[a - 1][c].getAttack() + "/" + card[a - 1][c].getDefense());
-            System.out.println(card1 + " takes " + card[a-1][c].getAttack() + " damage");
-            System.out.println(card2 + " takes " + card[a][c].getAttack() + " damage");
+            if (card[a - 1][c] == null) {
+                System.out.println("nulled");
+                String card1 = card[a][c].toString();
+                System.out.println(card1 + " has attack/defense " + card[a][c].getAttack() + "/" + card[a][c].getDefense());
+                System.out.println(card1 + " deals " + card[a][c].getAttack() + " damage player 1!");
+                p1HP = p1HP - card[a][c].getAttack();
+                buttons[0][5].setText(p1HP+"");
+            } else {
+                String card1 = card[a][c].toString();
+                String card2 = card[a - 1][c].toString();
+                System.out.println(card1 + " has attack/defense " + card[a][c].getAttack() + "/" + card[a][c].getDefense());
+                System.out.println(card2 + " has attack/defense " + card[a - 1][c].getAttack() + "/" + card[a - 1][c].getDefense());
+                System.out.println(card1 + " takes " + card[a-1][c].getAttack() + " damage");
+                System.out.println(card2 + " takes " + card[a][c].getAttack() + " damage");
             
-            boolean card1dead = false;
-            boolean card2dead = false;
-            if (card[a][c].getAttack() >= card[a - 1][c].getDefense()) {
-                System.out.println(card2 + " dies");
-                card2dead = true;
-            }
-            if (card[a][c].getDefense() <= card[a - 1][c].getAttack()) {
-                System.out.println(card1 + " dies");
-                card1dead = true;
-            }
+                boolean card1dead = false;
+                boolean card2dead = false;
+                if (card[a][c].getAttack() >= card[a - 1][c].getDefense()) {
+                    System.out.println(card2 + " dies");
+                    card2dead = true;
+                }
+                if (card[a][c].getDefense() <= card[a - 1][c].getAttack()) {
+                    System.out.println(card1 + " dies");
+                    card1dead = true;
+                }
             
-            //This should make combat matter.
-            if (card2dead) {
-                buttons[a-1][c].setIcon(new ImageIcon("NoCard.png"));
-                card[a-1][c] = null;
-            }
-            if (card1dead) {
-                buttons[a][c].setIcon(new ImageIcon("NoCard.png"));
-                card[a][c] = null;
+                //This should make combat matter.
+                if (card2dead) {
+                    buttons[a-1][c].setIcon(new ImageIcon("NoCard.png"));
+                    card[a-1][c] = null;
+                }
+                if (card1dead) {
+                    buttons[a][c].setIcon(new ImageIcon("NoCard.png"));
+                    card[a][c] = null;
+                }
             }
         }
     }
@@ -134,9 +162,9 @@ class ButtonListener implements ActionListener {
 
     public void actionPerformed(ActionEvent e) {
         int rows = 4;
-        int columns = 5;
+        int columns = 6;
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
+            for (int j = 0; j < columns-1; j++) {
                 if (e.getActionCommand().equals("Button"  + i + "," + j)) {
                     if (i == 3 || i == 0) {
                         buttonGame.moveCard(i,j);
